101d506a98d4ea0df33dc246aa9d8e1ca86ea636
kishor-pandit <kishorpandit309@gmail.com>
2024-12-09 21:51:50 +0545

    first commit

---
+++ [pratice.js
@@ -0,0 +1,8 @@
+const  student = {
+name:"kishor",
+age:"23",
+isfollow: false,
+};
+ 
+
+console.log(student.name);
---
+++ custom.js
@@ -0,0 +1,14 @@
+var id ="1234"
+ let user_name="Kishor pandit"
+let class_name="BCSIT"
+ 
+
+ document.getElementById("id").innerHTML=id
+ document.getElementsByName("user_name").innerHTML=user_name
+document.getElementsByClassName("class_name").innerHTML=class_name
+ document.getElementsByTagName("").innerHTML=tagname
+ document.querySelector("").innerHTMl=querySelector
+ Document.querySelectorAll("").innerHTML=selecterAll
+ console.log(id)
+ console.log(user_name)
+ console.log(class_name)
